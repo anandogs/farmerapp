b0fd15196bf98b5d50c012af0f153edc108a3046
Anando <anandoghose@gmail.com>
2020-04-19 14:28:38 +0530

    issue

--- farmer/settings.py
+++ farmer/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'epxz5=bdo29_u6dc$6-766=hswc195mg*4v+i+p)+@k%icw5kz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
